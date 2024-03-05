55d7ecbd95f9cac36a4bddc640fbc4d400316950
YrChek <y.chekmaev@mail.ru>
2024-03-05 22:17:18 +0300

    first_name

--- src/conponents/Header/Header.jsx
+++ src/conponents/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     <div className={'header'}>
       <Button onClick={onClose}>Закрыть</Button>
       <span className={'username'}>
-        {user ? user : 'no user'}
+        {user ? user.first_name : 'no user'}
       </span>
     </div>
   )
